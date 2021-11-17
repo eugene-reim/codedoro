b8344d267772a457fad194427d3b10a2fc9eb1e3
Eugene Reim <eugene.reim@gmail.com>
2021-11-17 15:19:16 +0300

    Added aria-labels for buttons to improve SEO

--- src/components/titlebar.svelte
+++ src/components/titlebar.svelte
@@ -2,9 +2,14 @@
     import {
         open
     } from '../components/create-task-modal.svelte';
+
+    function closeWindow(){
+        window.open('', '_self', ''); 
+        window.close();
+    }
 </script>
 <nav class="titlebar flex w-full bg-gray-700 p-3 justify-between text-white items-center">
-    <button class="exit-btn cursor-pointer">
+    <button class="exit-btn cursor-pointer" aria-label="Exit app" on:click="{closeWindow}">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 pointer-events-none select-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
         </svg>
@@ -12,7 +17,7 @@
     <h1 class="title font-bold text-xl pointer-events-none select-none">
         Codedoro
     </h1>
-    <button class="new-btn cursor-pointer" on:click={open}>
+    <button class="new-btn cursor-pointer" on:click={open} aria-label="Create task">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 pointer-events-none select-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
         </svg>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,18 +30,18 @@
             Create Task to start
         </p>
         <div class="countdown-controls flex justify-between items-center pl-10 pr-10">
-            <button class="reset-btn text-gray-500">
+            <button class="reset-btn text-gray-500" aria-label="Restart session">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
                 </svg>
             </button>
-            <button class="pause-btn text-white bg-yellow-300 rounded-full p-2 transform duration-700">
+            <button class="pause-btn text-white bg-yellow-300 rounded-full p-2 transform duration-700" aria-label="Pause session">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 pointer-events-none select-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
             </button>
-            <button class="stop-btn text-gray-500">
+            <button class="stop-btn text-gray-500" aria-label="Cancel session">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z" />
