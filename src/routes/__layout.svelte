f26395f1c3151eda4e010ea9f2d154d48a1bb445
eugene-reim <eugene.reim@gmail.com>
2024-12-01 13:37:56 +0300

    Revert "Add PWA support with service worker and offline capabilities"
    This reverts commit 85888acb9339f1a0efe7e496e9e80407ef581d10.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,7 @@
 <script lang="javascript">
     import "../app.css";
-    import { onMount } from 'svelte';
-    import { registerSW } from '$lib/pwa';
     import NavBar from "../components/navbar.svelte";
     import TitleBar from "../components/titlebar.svelte";
-
-    onMount(() => {
-      registerSW();
-    });
 </script>
 
 <section class="main-screen h-full w-full flex flex-col bg-gray-800 md:max-h-[700px]">
