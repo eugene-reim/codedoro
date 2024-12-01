d2ace4582a71fe103d5d3911773fcec4a5a2c986
eugene-reim <eugene.reim@gmail.com>
2024-12-01 20:31:46 +0300

    fix: remove SvelteKit config from svelte.config.js
    Simplified svelte.config.js to work with vanilla Svelte instead of SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
   preprocess: [
     preprocess({
       postcss: true,
